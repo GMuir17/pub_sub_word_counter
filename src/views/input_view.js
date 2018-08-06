b56fdf1f48b14ec6bf72ef2fb86188d7208dd697
Gary Muir <garymuir17@gmail.com>
2018-08-06 15:25:01 +0100

    sets up input_view.js

---
+++ src/views/input_view.js
@@ -0,0 +1,13 @@
+const PubSub = require("../helpers/pub_sub.js");
+
+const InputView = function () {};
+
+InputView.prototype.bindEvents = function () {
+  const input = document.querySelector("#text");
+  input.addEventListener("input", (evt) => {
+    const inputtedText = evt.target.value;
+    console.log(inputtedText);
+  });
+};
+
+module.exports = InputView;
